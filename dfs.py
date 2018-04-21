6ccfe2b5f1ee2826b57bf97b1d3e350ba62b57c7
Andrej <cermak_>
2018-04-21 16:33:38 +0200

    ADDED simple dfs fucntion

--- dfs.py
+++ dfs.py
@@ -3,10 +3,14 @@ MIT constraint satisfaction problem
 
 '''
 class Node:
-    def __init__(self, name, domain, vertexes):
-        self.vertexes = vertexes
+    def __init__(self, domain, constraints, name):
+        self.constraints = constraints
         self.domain = domain
+        self.assigned_value = None
         self.name = name
+        self.free = []
+        for i in range(len(domain)):
+            self.free.append(True)
 
 class Graph(object):
     def __init__(self, domain, graph_dict=None):
@@ -18,11 +22,54 @@ class Graph(object):
             graph_dict = {}
         self.__graph_dict = graph_dict
         self.domain = domain
-        self.nodes = []
+        self.nodes = {}
         print graph_dict
         for key in graph_dict:
-            self.nodes.append(Node(key, self.domain, graph_dict[key]))
+            self.nodes[key]=(Node(self.domain, graph_dict[key], key))
 
+    def dfs(self):
+        self.nodes['T'].domain = [1]
+        self.nodes['T'].assigned_value = 1
+        order1 = ["T","N","B","C", "P","S","L"]
+        order = ["T","L","B","C", "S","P","N"]
+        it = 0
+        x = 0
+        while it<len(order):
+
+            node = self.nodes[order[it]]
+            # backtracking
+            if node.assigned_value is not None and not it == 0:
+                start = node.assigned_value
+                node.assigned_value = None
+
+                # find next possible value
+                for i in range(start, len(node.free)):
+                    if node.free[i]:
+                        node.assigned_value = i + 1
+                        break
+                print "backtracking end ", node.name, node.assigned_value
+
+            else:
+                # cross out all non-possible values for current node
+                for const in node.constraints:
+                    if self.nodes[const].assigned_value is not None:
+                        node.free[self.nodes[const].assigned_value-1] = False
+
+                # find lowest possible value
+                for i in range(len(node.free)):
+                    if node.free[i]:
+                        node.assigned_value = i+1
+                        break
+                print node.name, node.assigned_value
+
+            # no possible value, backtrack
+            if node.assigned_value is None:
+                for i in range(len(node.free)):
+                    node.free[i] = True
+                it -= 1
+
+            else:
+                it += 1
 
 g = {"N": ["T","P","L","C","B"],
      "B": ["N","C"],
@@ -32,8 +79,16 @@ g = {"N": ["T","P","L","C","B"],
      "L": ["C","P","N","T"],
      "T": ["N","L"]
      }
+g1 = {"N": ["T","P","L","C","B","S"],
+     "B": ["N","C"],
+     "C": ["B", "N", "P", "L","T","S"],
+     "P": ["S", "C","N","L","T"],
+     "S": ["P","N","C"],
+     "L": ["C","P","N","T"],
+     "T": ["N","L","C","P"]
+     }
 domain = [1,2,3,4]
-graph = Graph(domain, g)
-
-for x in graph.nodes:
-    print x.name, x.domain, x.vertexes
+graph = Graph(domain, g1)
+graph.dfs()
+for name,node in graph.nodes.iteritems():
+    print name,node.assigned_value,node.domain, node.constraints,  node.free
